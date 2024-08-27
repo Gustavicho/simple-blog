a75378ff8dfa3d1c75973299f3258ab0eb18f976
Gustavo Oliveira <gustavoooliveiradecarvalho@gmail.com>
2024-08-27 11:11:27 -0400

    feat: add new components - change some clrs & etc

---
+++ resources/views/components/section-title.blade.php
@@ -0,0 +1,6 @@
+<div class="flex items-center gap-2 mb-4">
+    <span class="w-2 h-2 bg-white"></span>
+    <h2 class="text-xl font-bold">
+        {{ $slot }}
+    </h2>
+</div>
---
+++ resources/views/components/tag.blade.php
@@ -0,0 +1,16 @@
+@props(['size' => 'md'])
+
+@php
+    $classes = 'inline-flex items-center rounded-full bg-white/20 text-white transition duration-150 ease-in-out
+        hover:bg-white/40 focus:outline-none focus:ring-2 focus:ring-blue-500 ';
+    match ($size) {
+        'sm' => ($classes .= ' text-xs px-2.5 py-0.5'),
+        'md' => ($classes .= ' text-sm px-3 py-1.5'),
+        'lg' => ($classes .= ' text-base px-4 py-2'),
+        default => ($classes .= ' text-xs px-2.5 py-0.5'),
+    };
+@endphp
+
+<a {{ $attributes->merge(['class' => $classes]) }}>
+    {{ $slot }}
+</a>
