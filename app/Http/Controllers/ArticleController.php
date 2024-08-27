ab7c553d399fff9c35ba9e61e99aff1c761aee24
Gustavo Oliveira <gustavoooliveiradecarvalho@gmail.com>
2024-08-27 13:38:17 -0400

    feat: first router & controller

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Article;
 
 class ArticleController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('home', ['articles' => Article::query()->with('category', 'tags')->get()]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\Tag;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        'articles' => Article::query()->with(['category', 'tags'])->get(),
+        'tags' => Tag::all(),
+        'categories' => Category::all(),
+    ]);
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
