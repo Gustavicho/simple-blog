6155c2d94bdf7b032d2f324edf5f7c4b82c29428
Gustavo Oliveira <gustavoooliveiradecarvalho@gmail.com>
2024-08-27 13:34:54 -0400

    feat: create home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,17 +2,23 @@
     <div class="space-y-12">
         <section>
             <x-section-title>Categories</x-section-title>
-            @for ($i = 0; $i < 5; $i++)
-                <x-tag href="#" size="md">Científico</x-tag>
-            @endfor
+            @foreach ($categories as $category)
+                <x-tag :tag="$category" for="categories" />
+            @endforeach
         </section>
 
         <section>
             <x-section-title>Tags</x-section-title>
+            @foreach ($tags as $tag)
+                <x-tag :$tag />
+            @endforeach
         </section>
 
         <section>
             <x-section-title>Articles</x-section-title>
+            @foreach ($articles as $article)
+                <x-article-card :article="$article" />
+            @endforeach
         </section>
     </div>
 </x-app-layout>
