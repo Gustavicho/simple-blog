afd5c0f3107e59acaa3d11c8c88d167dafe06345
Gustavo Oliveira <gustavoooliveiradecarvalho@gmail.com>
2024-08-27 09:59:45 -0400

    fix: tag tes approach

--- tests/Unit/CategoryTest.php
+++ tests/Unit/CategoryTest.php
@@ -5,11 +5,8 @@ use App\Models\Category;
 
 it('has many articles', function () {
     $category = Category::factory()->create();
-    $arts = Article::factory(3)->create();
-
-    foreach ($arts as $art) {
-        $art->addCategory('laravel');
-    }
+    $arts = Article::factory()->count(3)->make();
+    $category->articles()->saveMany($arts);
 
     expect($category->articles)->toHaveCount(3);
 });
