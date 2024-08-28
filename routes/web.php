56f4ded0b090452b911e9c7db825f4ba86daf8bf
Gustavo Oliveira <gustavoooliveiradecarvalho@gmail.com>
2024-08-28 09:49:48 -0400

    fix: conflit in artciles.create & articles.show

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,28 @@ use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/about', 'about')->name('about');
-Route::get('/', [ArticleController::class, 'index'])->name('home');
 
 // search
 Route::get('/tags/{tag:name}', TagController::class);
 Route::get('/categories/{category:name}', CategoryController::class);
+
+Route::controller(ArticleController::class)->group(function () {
+    Route::get('/', 'index')->name('articles.index');
+    Route::get('/articles/create', 'create')
+        ->middleware('auth')->name('articles.create');
+
+    Route::post('/articles', 'store')
+        ->middleware('auth')->name('articles.store');
+
+    Route::get('/articles/{article}', 'show')->name('articles.show');
+    Route::get('/articles/{article}/edit', 'edit')
+        ->middleware('auth')->name('articles.edit');
+
+    Route::patch('/articles/{article}', 'update')
+        ->middleware('auth')->name('articles.update');
+    Route::delete('/articles/{article}', 'destroy')
+        ->middleware('auth')->name('articles.destroy');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
