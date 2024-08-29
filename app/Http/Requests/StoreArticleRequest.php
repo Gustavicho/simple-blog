2e8155ce9be51b205c9ae8198ee772510207b666
Gustavo Oliveira <gustavoooliveiradecarvalho@gmail.com>
2024-08-29 11:18:13 -0400

    feat: add form request with authorization

---
+++ app/Http/Requests/StoreArticleRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Article;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rules\File;
+
+class StoreArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Gate::allows('create', Article::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:255', 'unique:articles,title'],
+            'content' => ['required'],
+            'image' => ['nullable', File::image()],
+            'category' => ['nullable', 'string', 'max:255'],
+            'tags' => ['nullable'],
+        ];
+    }
+
+    public function mensage(): array
+    {
+        return [
+            'title' => ['required' => 'Must have a title', 'unique' => 'That\'s title already exists'],
+            'content.required' => ['Must have a content'],
+            'image.image' => ['Only images files are allowed'],
+        ];
+    }
+}
