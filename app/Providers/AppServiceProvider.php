50216a3d2aededd2eb3c10261d2905bd2b6be75f
Gustavo Oliveira <gustavoooliveiradecarvalho@gmail.com>
2024-08-29 11:19:44 -0400

    feat: add authorization to article with policy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
     }
 
     /**
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::preventLazyLoading();
+        Gate::guessPolicyNamesUsing(function ($modelClass) {
+            return 'App\Policies\\'.class_basename($modelClass).'Policy';
+        });
     }
 }
